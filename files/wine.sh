c63e8f11f5e83c5dad44ff681956ba23463c6b12
vallnoob <46414928+vallnoob@users.noreply.github.com>
2019-07-24 13:27:13 +0300

    Add files via upload

---
+++ files/wine.sh
@@ -0,0 +1,11 @@
+cd ~
+sudo dpkg --add-architecture i386
+wget -nc https://dl.winehq.org/wine-builds/winehq.key
+sudo apt-key add winehq.key
+sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu bionic main'
+sudo apt -y --fix-broken install
+wget https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/xUbuntu_18.04/amd64/libfaudio0_19.07-0~bionic_amd64.deb
+wget https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/xUbuntu_18.04/i386/libfaudio0_19.07-0~bionic_i386.deb
+sudo dpkg -i libfaudio0_19.07-0~bionic_amd64.deb libfaudio0_19.07-0~bionic_i386.deb
+rm libfaudio0_19.07-0~bionic_amd64.deb libfaudio0_19.07-0~bionic_i386.deb winehq.key
+sudo apt install -y --install-recommends winehq-staging
