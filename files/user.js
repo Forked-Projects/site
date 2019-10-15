a33c09a2f68f80d7f19579b7ee7aafcea786f927
vallnoob <46414928+vallnoob@users.noreply.github.com>
2019-10-15 09:56:38 +0300

    Update 15-окт.-2019_09-57

---
+++ files/user.js
@@ -0,0 +1,6 @@
+user_pref("general.autoScroll", true);
+user_pref("general.smoothScroll", false);
+user_pref("browser.download.autohideButton", false);
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("intl.locale.requested", "ru");
+user_pref("reader.parse-on-load.enabled", false);
