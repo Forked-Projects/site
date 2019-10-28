edf3e2ee0ef65221c3984cf2271b11800eb7c16e
vallnoob <46414928+vallnoob@users.noreply.github.com>
2019-10-28 22:07:01 +0300

    Update 28-окт.-2019_22-08

--- files/user.js
+++ files/user.js
@@ -4,3 +4,4 @@ user_pref("browser.download.autohideButton", false);
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("intl.locale.requested", "ru");
 user_pref("reader.parse-on-load.enabled", false);
+user_pref("extensions.pocket.enabled", false);
