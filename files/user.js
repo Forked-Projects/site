a95e3e11dfc3416b2a3fa5828a5a9fa9db9feca5
vallnoob <46414928+vallnoob@users.noreply.github.com>
2019-12-30 12:07:38 +0300

    Delete user.js

--- files/user.js
+++
@@ -1,7 +0,0 @@
-user_pref("general.autoScroll", true);
-user_pref("general.smoothScroll", false);
-user_pref("browser.download.autohideButton", false);
-user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-user_pref("intl.locale.requested", "ru");
-user_pref("reader.parse-on-load.enabled", false);
-user_pref("extensions.pocket.enabled", false);
