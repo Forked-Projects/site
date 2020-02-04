dc7eede11c3dd529031f4c7798aeab9218247387
vallnoob <46414928+vallnoob@users.noreply.github.com>
2020-02-04 22:01:38 +0300

    Update 05-Feb-2020_01-01

--- files/calc2.c
+++ files/calc2.c
@@ -1,34 +1,43 @@
 #include <stdio.h>
-
 #define MAX 100;
 
 int ops[MAX];
 char line[MAX];
 
+int gline();
+int getop();
+
 int main()
 {
+
+  while(gline() > 0) {
+    for(i = 0; (op = getop()) != '\0' && i < MAX; i++)
+      ops[i] = op;
+  }
 }
 
-void getops()
+int gline()
 {
   char c;
-  int i, length, sign;
+  int i, length;
 
-  for(i = 0; (c = getchar()) != '\n' && c != EOF; i++)
+  for(i = 0; (c = getchar()) != '\n' && c != EOF && i < MAX; i++)
     if(c == EOF)
       return 0;
     else
       line[i] = c;
   line[i] = '\0';
   length = i;
+}
 
-  for(i = 0; line[i] != '\0'; i++)
+int getop()
+{
     while(line[i] != ' ' || line[i] != '\t')
       ;
     if(line[i] == '-' && line[i + 1] >= '0' && line[i + 1] <= '9')
       sign = -1;
     else
       sign = 1;
-    
+    if(line[i] >= '0' && line [i] <= '9')
+      ops[j] = ops[j] * 10 + line[i];
 }
-
