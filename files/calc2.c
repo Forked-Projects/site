6e9e3054cf849357af7318a47d795af6ed316800
vallnoob <46414928+vallnoob@users.noreply.github.com>
2020-02-05 19:58:11 +0300

    Update 05-Feb-2020_22-56

--- files/calc2.c
+++ files/calc2.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-#define MAX 100;
+#define MAX 100
+#define ERR 1
+#define EOL 1
 
 int ops[MAX];
 char line[MAX];
@@ -9,35 +11,49 @@ int getop();
 
 int main()
 {
+  int i;
 
   while(gline() > 0) {
-    for(i = 0; (op = getop()) != '\0' && i < MAX; i++)
-      ops[i] = op;
+    for(i = 0; i < MAX; i++)
+      ops[i] = getop();
   }
+  for(i = 0; i < 10; i++)
+    printf("%d\n", ops[i]);
 }
 
 int gline()
 {
   char c;
-  int i, length;
+  int i;
 
-  for(i = 0; (c = getchar()) != '\n' && c != EOF && i < MAX; i++)
-    if(c == EOF)
-      return 0;
-    else
-      line[i] = c;
+  for(i = 0; (c = getchar()) != '\n' && c != EOF && i < MAX; i++) {
+    line[i] = c;
+  }
+  if(c == EOF)
+    return 0;
   line[i] = '\0';
-  length = i;
+  return i;
 }
 
 int getop()
 {
-    while(line[i] != ' ' || line[i] != '\t')
-      ;
+  static int i;
+  int num, sign;
+  sign = 1;
+  num = 0;
+  
+  while(line[i] == ' ' || line[i] == '\t')
+    i++;
+  while(line[i] != ' ' && line[i] != '\t' && line[i] != '\0') {
     if(line[i] == '-' && line[i + 1] >= '0' && line[i + 1] <= '9')
       sign = -1;
-    else
-      sign = 1;
     if(line[i] >= '0' && line [i] <= '9')
-      ops[j] = ops[j] * 10 + line[i];
+      num = num * 10 + (line[i] - '0');
+    else {
+      printf("illegal number\n");
+    }
+    i++;
+  }
+  num = sign * num;
+  return num;
 }
