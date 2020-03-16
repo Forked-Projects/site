c9839c605600d6c53aba13b301089a2d3d28f1b2
vallnoob <46414928+vallnoob@users.noreply.github.com>
2020-03-16 21:37:56 +0300

    Update 16-марта-2020_21-38

--- files/vkchat.sh
+++ files/vkchat.sh
@@ -2,22 +2,24 @@ token=$1
 head='https://api.vk.com/method'
 v=5.103
 
-response=$(
-  fetch -q -o - $head/messages.getConversations?count=8\&extended=1\&access_token=$token\&v=$v |\
-  sed $'s/,"/\\\n"/g; s/:{/\\\n/g; s/},{/\\\n/g; s/\"profiles\":\\[{/\"profiles\":\\\n/g'
-)
-
-profiles=$(
-  echo "$response" |\
-  sed '1,/"profiles":/d; /"groups":/,/$/d' |\
-  egrep '^"id":|^"first_name":|^"last_name":|^"online":'
-)
-
-conversations=$(
-  echo "$response" |\
-  sed '/"attachments":\[\]/,/"conversation"/d; s/}$//' |\
-  egrep '^"text":|^"local_id":|^"attachments":|^"unread_count":|^"fwd_messages":\[{'
-)
+update() {
+  response=$(
+    fetch -q -o - $head/messages.getConversations?count=8\&extended=1\&access_token=$token\&v=$v |\
+    sed $'s/,"/\\\n"/g; s/:{/\\\n/g; s/},{/\\\n/g; s/\"profiles\":\\[{/\"profiles\":\\\n/g'
+  )
+  
+  profiles=$(
+    echo "$response" |\
+    sed '1,/"profiles":/d; /"groups":/,/$/d' |\
+    egrep '^"id":|^"first_name":|^"last_name":|^"online":'
+  )
+  
+  conversations=$(
+    echo "$response" |\
+    sed '/"attachments":\[\]/,/"conversation"/d; s/}$//' |\
+    egrep '^"text":|^"local_id":|^"attachments":|^"unread_count":|^"fwd_messages":\[{'
+  )
+}
 
 getname() {
   echo "$profiles" |\
@@ -28,28 +30,41 @@ getname() {
   tr -d '"'
 }
 
-getlist() {
-  echo -e '**************************************************\n'
+getids() {
   chatnum=0 
-  while read -r line
+  while read line
+  do
+    if [ $(echo $line | grep local_id) ]
+      then
+        chatnum=$(($chatnum+1))
+        id=$(echo $line | sed 's/"local_id"://')
+	idnum=$(printf "id$chatnum")
+        export $idnum=$id
+    fi
+  done <<END
+  $(echo "$conversations")
+END
+}
+
+getlist() {
+  echo '********************************************************************************'
+  chatnum=0 
+  while read line
   do
     if [ $(echo $line | grep local_id) ]
       then
         chatnum=$(($chatnum+1))
 	echo ""
         printf "$chatnum "
-        id=$(echo $line | sed 's/"local_id"://')
-        getname "$id"
-	idnum=$(printf "id$chatnum")
-        export $idnum=$id
-     else
+	eval getname $(printf $"id$chatnum")
+      else  
         echo $line |\
 	sed '/"unread_count":/s/$/!\]/; s/"unread_count":/ \[/' |\
 	sed 's/"attachments":\[{"type":"wall/: @ Запись на стене/' |\
 	sed 's/"attachments":\[{"type":"photo/: >> Изображение/' |\
 	sed 's/"attachments":\[{"type":"sticker/: Стикер/' |\
 	sed 's/"fwd_messages":\[{.*/: @ Сообщение/' |\
-	sed 's/"text":"/:  /; s/"$//; s/\\n.*//g' |\
+	sed 's/"text":"/:  /; s/"$//' |\
         grep -v '^: *$' |\
 	tr -d '\n'
     fi
@@ -58,25 +73,61 @@ getlist() {
 END
 }
 
-getchat() {
+gethistory() {
   fetch -q -o - $head/messages.getHistory?user_id=$1\&access_token=$token\&v=$v |\
   sed $'s/,"/\\\n"/g'
+  echo ''
+  echo '--------------------------------------------------------------------------------'
+  echo -n '>> : '
 }
 
-getlist | cut -c -$(tput columns)
-echo ""
-while read char
-do
-  if [ $(echo $char | egrep [0-9]) ]
-    then
-      getchat $(eval echo $(printf $"id$char"))
-  fi
-  if [ $char == r ]
-    then
-      getlist
-  fi
-  if [ $char == q ]
-    then
-      exit
-  fi
-done
+getdialog() {
+  curid=$1
+  gethistory $curid
+  while read message
+  do
+    if [ "$message" == r ] || [ "$message" == к ]
+      then
+	gethistory $curid
+    elif [ "$message" == q ] || [ "$message" == й ]
+      then
+        mainlist
+    else
+      echo "$message"
+      random=$(cat /dev/urandom | head -1 | od -D -N4 -vAn | tr -d ' ')
+      fetch -q -o - $head/messages.send?user_id=$curid\&message="$message"\&random_id=$random\&access_token=$token\&v=$v |\
+      grep -v "response"
+      gethistory $curid
+    fi
+  done
+} 
+
+mainlist() {
+  update
+  getids
+  getlist | cut -c -$(tput columns)
+  echo ""
+  echo ""
+  while read char
+  do
+    if [ $(echo "$char" | egrep [0-9]) ]
+      then
+        curid=$(eval printf $"id$char")
+        getdialog $curid
+    fi
+    if [ "$char" == r ] || [ "$char" == к ]
+      then
+	update
+        getids
+        getlist | cut -c -$(tput columns)
+        echo ""
+        echo ""
+    fi
+    if [ "$char" == q ] || [ "$char" == й ]
+      then
+        exit
+    fi
+  done
+}
+
+mainlist
